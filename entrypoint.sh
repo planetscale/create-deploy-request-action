d83f4ad063e1d71e4ea466e4f2ff97cf6fe653e0
Brian Morrison II <brian@brianmorrison.me>
2023-03-10 14:52:26 -0500

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
 command="pscale deploy-request create $1 $2 --org $3 \
-  --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME"
+  --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME -f json"
 
 if [ -n "$4" ];then
   command="$command --deploy-to $4"
 fi
-
 
 CMDOUT=$(eval $command)
 
